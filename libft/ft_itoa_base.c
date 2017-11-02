a0b3367897f8da174579bedd34aaa4bb42831a9d
Stanislas Juery <sjuery@e1z2r8p23.42.us.org>
2017-11-02 15:10:07 -0700

    Changed itoa base

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -1,42 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sjuery <sjuery@student.42.us.org>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/25 12:41:35 by sjuery            #+#    #+#             */
-/*   Updated: 2017/11/02 14:50:17 by sjuery           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-char	*ft_itoa_base(int value, int base, int lowercase)
+#include <stdlib.h>
+#include <stdio.h>
+int		ft_abs(int nb)
 {
-	int		flag;
-	int		len;
+	if (nb < 0)
+		nb = -nb;
+	return (nb);
+}
+char	*ft_itoa_base(int value, int base)
+{
 	char	*str;
-	int		v;
-
-	if (base < 2 && base > 36)
+	int		size;
+	char	*tab;
+	int		flag;
+	int		tmp;
+	flag = 0;
+	size = 0;
+	tab = "0123456789ABCDEF";
+	if (base < 2 || base > 16)
 		return (0);
-	flag = (value < 0 && base == 10) ? 1 : 0;
-	len = 1 + flag;
-	v = value;
-	while (v /= base)
-		len++;
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	str[0] = '-';
-	str[len] = 0;
-	while (len-- > flag)
+	if (value < 0 && base == 10)
+		flag = 1;
+	tmp = value;
+	while (tmp /= base)
+		size++;
+	size = size + flag + 1;
+	str = (char *)malloc(sizeof(char) * size + 1);
+	str[size] = '\0';
+	if (flag == 1)
+		str[0] = '-';
+	while (size > flag)
 	{
-		v = (value < 0) ? -(value % base) : value % base;
-		if(lowercase == 1)
-			str[len] = (v > 9) ? v + 'a' - 10 : v + '0';
-		else
-			str[len] = (v > 9) ? v + 'A' - 10 : v + '0';
-		value /= base;
+		str[size - 1] = tab[ft_abs(value % base)];
+		size--;
+		value /=base;
 	}
 	return (str);
 }
