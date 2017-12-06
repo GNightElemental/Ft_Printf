d4393ef670ed85ee06a1ba99566a63bc4e6c8f77
Stanislas Juery <sjuery@e1z2r9p43.42.us.org>
2017-12-06 03:33:08 -0800

    stopped doing bonuses, might still do *

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sjuery <sjuery@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 23:50:01 by sjuery            #+#    #+#             */
-/*   Updated: 2017/12/06 02:46:53 by sjuery           ###   ########.fr       */
+/*   Updated: 2017/12/06 03:03:05 by sjuery           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 
 int	main()
 {
-	ft_printf("%A\n", "test this out please ");
-	printf("%A", "test this out please ");
+	ft_printf("%A\n", 134.6);
+	printf("%A", 134.629658);
 }
